2835151d8e23a01ad0da7a4f64865a8dda273509
RAINYCHELO <marcelo.delahozs@udea.edu.co>
2021-04-20 14:26:31 -0500

    add player class

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ from Game import *
 
 
 
-def main():
-    game = Game()
+def main(game=None):
+    """game = Game()
     ip = input("Direccion ip del servidor: ")
     nick = input("nickname: ")
-    choosed_pokemon = input("pokemon a usar: ")
-
-
+    choosed_pokemon = input("pokemon a usar: ")"""
+    
+    
     while not game.stopped:
         # Main pokemon battle loop
         # First ask for the commands
